164e705b97c667847ad2c94011a2856637fe1980
Shrawan Shinde <139120753+shrawanfv@users.noreply.github.com>
2024-01-18 11:25:48 +0530

    [ADD] - Added all plugins

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,35 @@
-module.exports = function(api) {
+/*
+ * ************************************************************************************************
+ * ************************************************************************************************
+ * Project: talk2notes
+ * File Name: babel.config.js
+ * Created Date: Wednesday, January 17th 2024, 1:37:40 pm
+ * Author: Shrawan Shinde
+ * -----
+ * Last Modified: Wednesday, January 17th 2024, 1:37:40 pm
+ * Modified By:  Shrawan Shinde
+ * -----
+ * Copyright (c) 2024 RajeAi
+ * ************************************************************************************************
+ * ************************************************************************************************
+ */
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+
+    plugins: [
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+    ],
   };
 };
